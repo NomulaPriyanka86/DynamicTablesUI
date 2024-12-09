6e04473572d230353b8f815dc6cd447bf021cd6e
Vikas <vikas.p@bluboy.in>
2024-12-09 20:15:14 +0530

    made changes for securing pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,27 @@ import { tenantName } from './services/apiService';
 import 'primereact/resources/themes/saga-blue/theme.css';  // Choose a theme
 import 'primereact/resources/primereact.min.css';  // Core CSS
 import 'primeicons/primeicons.css';  // Icons CSS
+import { AuthProvider, RequireAuth } from './components/AuthProvider'
+import {Login} from './components/Login'
+
 function App() {
   return (
-    <Router> {/* Wrap your routes in a Router */}
-      <Routes>
-        <Route path="/dynamic-table/:pageTitle" element={<DynamicTablesUI tenantName={tenantName} />} />
-        <Route path="/" element={<PageSchemas tenantName={tenantName} />} />
-        {/* <Route path="/dynamic-table" element={<DynamicTablesUI tenantName={tenantName} />} /> */}
-      </Routes>
-    </Router>
+    <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/dynamic-table/:pageTitle" element={
+            <RequireAuth>
+              <DynamicTablesUI tenantName={tenantName} />
+            </RequireAuth>
+          } />
+          <Route path="/" element={
+            <RequireAuth>
+              <PageSchemas tenantName={tenantName} />
+            </RequireAuth>
+            } />
+          {/* <Route path="/dynamic-table" element={<DynamicTablesUI tenantName={tenantName} />} /> */}
+        </Routes>
+      </AuthProvider>
   )
 }
 export default App
